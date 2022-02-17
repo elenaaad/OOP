ff907abfce7febf05f96e6e5fe8e3427eea2a0e5
elenaaad <94315701+elenaaad@users.noreply.github.com>
2022-02-17 10:56:16 +0200

    nr_perf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, world!\n";
-    return 0;
+int perfect (int x)
+{
+  int d, s = 0;
+  for (d=1; d<=x/2; d++)
+  {
+    if (x%d == 0) s+=d;
+    d++;
+  }
+
+  if (x==s) return 1;
+  return 0;
+ }
+
+ int main ()
+{
+ cin>>a>>b;
+ int i;
+ for (i=a; i<=b, i++)
+   if ( perfect(i) == 1)
+     cout<<i;
+
+return 0;
 }
